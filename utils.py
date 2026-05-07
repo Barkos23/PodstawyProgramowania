d50bdbc3821ea547dd1973467c3e06b7bdb7cea2
Bartosz Kościański <280108@student.pwr.edu.pl>
2026-05-07 12:24:54 +0200

    Poprawa błędów

--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
-# przykladowe funkcje utils.py
+""" Przykladowe funkcje utils.py """
 def add(a: int, b: int) -> int:
+    """Zwraca sumę dwóch liczb"""
     return a + b
 def subtract(a: int, b: int) -> int:
+    """Zwraca różnicę dwóch liczb"""
     return a - b
 def multiply(a: int, b: int) -> int:
+    """Zwraca iloczyn dwóch liczb"""
     return a * b
 def divide(a: int, b: int) -> float:
+    """Zwraca iloraz dwóch liczb"""
     return a / b
